f45444f40d43fc5d3dcc3d2a14edb3eff203e448
Ivan_Markushyn <vanuhamark@gmail.com>
2024-12-29 21:26:02 +0100

    test husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { TrpcProvider } from './lib/trpc'
 import { MainPage } from './pages'
 
 const App = () => {
+  if (Math.random()) console.log('test')
   return (
     <TrpcProvider>
       <MainPage />

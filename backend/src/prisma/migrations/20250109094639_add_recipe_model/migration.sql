ead7a2de4ea98360fb67a7ce28049cae5b3d07c6
Ivan_Markushyn <vanuhamark@gmail.com>
2025-01-09 10:50:48 +0100

    Add: prisma and recipe model

---
+++ backend/src/prisma/migrations/20250109094639_add_recipe_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);

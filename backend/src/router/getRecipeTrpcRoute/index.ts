5265a7e8cc9b9a7aa667f6d5fb1630f4bf309d52
Ivan_Markushyn <vanuhamark@gmail.com>
2025-01-13 15:28:05 +0100

    Update: get recipe route, use prisma

--- backend/src/router/getRecipeTrpcRoute/index.ts
+++ backend/src/router/getRecipeTrpcRoute/index.ts
@@ -1,9 +1,11 @@
-import { recipes, trpc } from '../../lib'
+import { trpc } from '../../lib'
 import { z } from 'zod'
 
 export const getRecipeTrpcRoute = trpc.procedure
   .input(z.object({ id: z.string() }))
-  .query(({ input }) => {
-    const recipe = recipes.find((recipe) => recipe.id === input.id)
-    return { recipe: recipe || null }
+  .query(async ({ input, ctx }) => {
+    const recipe = await ctx.prisma.recipe.findUnique({
+      where: { id: input.id },
+    })
+    return { recipe }
   })

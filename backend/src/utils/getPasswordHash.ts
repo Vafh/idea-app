1006598b6bccb914fc5931776414723d69f49af6
Ivan_Markushyn <vanuhamark@gmail.com>
2025-01-18 21:14:11 +0100

    Add: hash password

--- backend/src/utils/getPasswordHash.ts
+++ backend/src/utils/getPasswordHash.ts
@@ -1,6 +1,10 @@
+import { env } from '../lib/env'
 import crypto from 'crypto'
 
 const getPasswordHash = (password: string) =>
-  crypto.createHash('sha256').update(password).digest('hex')
+  crypto
+    .createHash('sha256')
+    .update(`${env.PASSWORD_SALT}${password}`)
+    .digest('hex')
 
 export default getPasswordHash
